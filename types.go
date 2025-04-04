1a8765c9710af2b4897f15aa652b296cee61680b
leonardoSerilli <leoser14592@gimail.com>
2025-04-04 16:20:53 +0200

    TSK-16 Main Core: cyberleaf plugin object

--- types.go
+++ types.go
@@ -3,3 +3,16 @@ package cyberleafSDK
 // A map where keys are plugin categories and values are lists of plugin names.
 // TODO must be accessible from Plugins
 type PluginsMap map[string][]string
+
+type CyberleafPluginMetadata struct {
+	// The name of the plugin
+	Name string
+	// The type of the plugin
+	Type string
+	// The version of the plugin
+	Version string
+	// Required plugins for this plugin
+	RequiredPlugins PluginsMap
+	// Accepted analysers for this plugin
+	AcceptedAnalysers []string
+}
