865edeedfa235b646609dc39c6690d8229424859
Leonardo-Serilli-Daedalus <leonardo.serilli@daedaluslab.it>
2025-05-06 16:33:48 +0200

    TSK-16 Main Core: json formatting

--- cyberleafChannelItem.go
+++ cyberleafChannelItem.go
@@ -5,54 +5,54 @@ import (
 	"sync"
 )
 
-// Represents ta single optimization for an issue by an optimizer
-type Optimization struct {
-	Optimizer   string
-	Description string
-	AST         *ast.File // Abstract Syntax Tree of the file
-	// TODO check if other fields are needed
+// Represents a solution for an issue by an optimizer
+type Solution struct {
+	Optimizer   string `json:"optimizer"`   // Name of the optimizer
+	Description string `json:"description"` // Description of the suggested solution
 }
 
 // Represents a single issue found by an analyser
 type Issue struct {
-	ID                string
-	Description       string
-	Optimizations     *[]Optimization // List of possible optimizations for this issue
-	optimizationMutex sync.Mutex      // Mutex to ensure thread safety when modifying Optimizations
-	// TODO check if other fields are needed
+	ID                string      `json:"id"`                  // Identifier for the issue
+	Row               int         `json:"row"`                 // Line number where the issue was found
+	Description       string      `json:"description"`         // Description of the issue
+	Solutions         *[]Solution `json:"solutions,omitempty"` // List of solutions for this issue by different optimizers (omitted if empty)
+	optimizationMutex sync.Mutex  `json:"-"`                   // Mutex to ensure thread safety when modifying Optimizations (excluded from JSON)
 }
 
+// Represents a full analysis result by one analyser
 type Analysis struct {
-	Analyser string   // Name of the analyser
-	Issues   []*Issue // List of issues found by the analyser
+	Analyser string   `json:"analyser"` // Name of the analyser
+	Issues   []*Issue `json:"issues"`   // List of issues found by the analyser
 }
 
 // Represents the item shared and updated by the plugins in the execution chain
 type CyberleafChannelItem struct {
-	Target        string      // Path to the file being processed
-	AST           *ast.File   // Abstract Syntax Tree of the file
-	Analysis      *[]Analysis // List of analyses performed on the item
-	analysisMutex sync.Mutex  // Mutex to ensure thread safety when modifying Analysis
+	ID            int         `json:"-"`        // ID to sort the items (excluded from JSON)
+	Target        string      `json:"target"`   // Path to the file being processed
+	AST           *ast.File   `json:"-"`        // Abstract Syntax Tree of the file (excluded from JSON)
+	Analysis      *[]Analysis `json:"analysis"` // List of analyses performed on the item
+	analysisMutex sync.Mutex  `json:"-"`        // Mutex to ensure thread safety when modifying Analysis (excluded from JSON)
 }
 
+// Adds a new analysis in a thread-safe way
 func (item *CyberleafChannelItem) AddAnalysis(newAnalysis Analysis) {
 	item.analysisMutex.Lock()         // Lock the mutex to ensure mutual exclusion
 	defer item.analysisMutex.Unlock() // Unlock the mutex when the function exits
 
-	// Append analysi
 	if item.Analysis == nil {
 		item.Analysis = &[]Analysis{}
 	}
 	*item.Analysis = append(*item.Analysis, newAnalysis)
 }
 
-func (issue *Issue) AddOptimization(newOptimization Optimization) {
+// Adds a new solution to an issue in a thread-safe way
+func (issue *Issue) AddOptimization(newOptimization Solution) {
 	issue.optimizationMutex.Lock()         // Lock the mutex to ensure mutual exclusion
 	defer issue.optimizationMutex.Unlock() // Unlock the mutex when the function exits
 
-	// Append optimization
-	if issue.Optimizations == nil {
-		issue.Optimizations = &[]Optimization{}
+	if issue.Solutions == nil {
+		issue.Solutions = &[]Solution{}
 	}
-	*issue.Optimizations = append(*issue.Optimizations, newOptimization)
+	*issue.Solutions = append(*issue.Solutions, newOptimization)
 }
